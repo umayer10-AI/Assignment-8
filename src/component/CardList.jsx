e3f4d1e6f43ab504dd92da37baf79dd53f647177
umayer10-AI <mdumayer10@gmail.com>
2026-04-29 20:13:36 +0600

    add

--- src/component/CardList.jsx
+++ src/component/CardList.jsx
@@ -1,12 +1,32 @@
+import { Button, Separator } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
+import { FaBangladeshiTakaSign } from 'react-icons/fa6';
 
 const CardList = ({p}) => {
     return (
-        <div>
-            <div>
-                <Image src={p.image} height={100} width={100} alt='photo'></Image>
+        <div className='p-4 rounded-xl space-y-1.5 shadow-xl'>
+            <div className='relative aspect-square'>
+                <Image src={p.image} fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className='rounded-xl object-cover' alt='photo'></Image>
             </div>
+            <h2 className='font-bold'>{p.name}</h2>
+                <div className='flex items-center justify-between'>
+                    <div className='flex items-center gap-2'>
+                        <p className='font-semibold'>weight: </p>
+                        <p className='font-bold'>{p.weight}KG</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <p className='font-semibold'>Age: </p>
+                        <p className='font-bold'>{p.age}</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <p className='font-semibold'>Price: </p>
+                    <p className='font-bold text-green-600 flex items-center'><FaBangladeshiTakaSign />{p.price}</p>
+                </div>
+            <Button className={'bg-linear-to-r from-green-500 via-blue-500 bg-green-500 w-full'}>Details</Button>
         </div>
     );
 };
