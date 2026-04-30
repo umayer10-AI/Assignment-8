1197021978e7147c1c2ab0992601cc761c319e84
umayer10-AI <mdumayer10@gmail.com>
2026-04-30 09:56:26 +0600

    add

--- src/component/ModalTask.jsx
+++ src/component/ModalTask.jsx
@@ -1,59 +1,64 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
 import {Envelope} from "@gravity-ui/icons";
 import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
 import React from 'react';
+import { BiEdit } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa6";
 
 const ModalTask = () => {
+
+    const a = async (e) => {
+        e.preventDefault()
+
+        const name = e.target.name.value
+        const image = e.target.url.value
+        
+        await authClient.updateUser({
+            image: "https://example.com/image.jpg",
+            name: "John Doe",
+        })
+    }
+
     return (
         <div>
             <Modal>
-      <Button variant="secondary">Open Contact Form</Button>
+      <Button variant="secondary"><BiEdit/>Update Profile</Button>
       <Modal.Backdrop>
         <Modal.Container placement="auto">
-          <Modal.Dialog className="sm:max-w-md">
+          <Modal.Dialog className="w-[90%] sm:max-w-md">
             <Modal.CloseTrigger />
             <Modal.Header>
-              <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
-                <Envelope className="size-5" />
-              </Modal.Icon>
-              <Modal.Heading>Contact Us</Modal.Heading>
-              <p className="mt-1.5 text-sm leading-5 text-muted">
-                Fill out the form below and we will get back to you. The modal adapts automatically
-                when the keyboard appears on mobile.
-              </p>
+              <div className="flex flex-col items-center gap-3">
+                <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
+                    <FaRegUser className="size-5" />
+                </Modal.Icon>
+                <Modal.Heading className="text-xl">Update User</Modal.Heading>
+              </div>
+
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form className="flex flex-col gap-4">
+                <form onSubmit={a} className="flex flex-col gap-4">
+
                   <TextField className="w-full" name="name" type="text">
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
                   </TextField>
-                  <TextField className="w-full" name="email" type="email">
-                    <Label>Email</Label>
-                    <Input placeholder="Enter your email" />
+                  <TextField className="w-full" name="url" type="text">
+                    <Label>Image URL</Label>
+                    <Input placeholder="Enter your URL" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
-                  </TextField>
-                  <TextField className="w-full" name="company">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
-                  </TextField>
-                </form>
-              </Surface>
-            </Modal.Body>
+
             <Modal.Footer>
               <Button slot="close" variant="secondary">
                 Cancel
               </Button>
-              <Button slot="close">Send Message</Button>
+              <Button type="submit" slot="close">Save</Button>
             </Modal.Footer>
+                </form>
+              </Surface>
+            </Modal.Body>
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
