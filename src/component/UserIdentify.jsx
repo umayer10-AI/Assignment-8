686e29c3ffe5f9db209ba377c5e9006806ca5dab
umayer10-AI <mdumayer10@gmail.com>
2026-04-30 08:45:34 +0600

    add

--- src/component/UserIdentify.jsx
+++ src/component/UserIdentify.jsx
@@ -1,13 +1,13 @@
 "use client"
 import { authClient } from '@/lib/auth-client';
 import { Avatar, Button } from '@heroui/react';
+import Link from 'next/link';
 import React from 'react';
 
 const UserIdentify = () => {
 
     const { data: session } = authClient.useSession()
     const user = session?.user
-    console.log(user)
 
     return (
         <div>
@@ -15,14 +15,16 @@ const UserIdentify = () => {
                 user? <div className='flex items-center gap-3'>
                     <h2 className='font-semibold hidden lg:flex'>{user?.name}</h2>
                     <Avatar size='sm'>
-                        <Avatar.Image alt="Umayer Ahmad" src={user?.image} />
+                        <Avatar.Image alt="Umayer Ahmad"
+                        referrerPolicy='no-referrer'
+                        src={user?.image} />
                         <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
                     </Avatar>
                     <Button onClick={async() => await authClient.signOut()} size='sm' variant='danger'>Log Out</Button>
                 </div>
                 : <div className='flex gap-2'>
-                    <Button className={'bg-linear-to-r from-blue-500 to-green-500'}>Register</Button>
-                    <Button variant='secondary'>Login</Button>
+                    <Link href={'/register'}><Button className={'bg-linear-to-r from-blue-500 to-green-500'}>Register</Button></Link>
+                    <Link href={'/login'}><Button variant='secondary'>Login</Button></Link>
                 </div>
             }
         </div>
